fa9557ed9a5f498ea3aab5824e59d87c39a8de73
HiddenCoder <jhpark@gmail.com>
2022-06-12 20:44:40 +0900

    [추가] : layout 추가  - header 추가  - footer 추가  - sidebar 추가  - css 추가

--- file_manager/src/App.vue
+++ file_manager/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
+    <main-side-bar></main-side-bar>
+    <div id="header">
+      <main-header/>
+    </div>
     <router-view></router-view>
+    <div id="footer">
+      <main-footer/>
+    </div>
   </div>
 </template>
 
 <script>
+  import MainFooter from './components/MainFooter.vue'
+  import MainHeader from './components/MainHeader.vue'
+  import MainSideBar from './components/MainSideBar.vue'
 
-export default {
-  name: 'App',
-}
+  export default {
+    components: { MainHeader, MainFooter, MainSideBar },
+    name: 'App',
+  }
 </script>
 
 <style>
