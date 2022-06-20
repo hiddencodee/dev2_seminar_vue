8ccdf23b80b7afc407a687d76ae79db2ce349167
Rainbow01734 <rainbow01734@gmail.com>
2022-06-20 17:06:22 +0900

    [수정] : 사이드 바 상단 이미지 추가

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -1,6 +1,29 @@
 <template>
-  <q-drawer show-if-above v-bind="leftDrawerOpen" side="left" bordered>
-    <!-- drawer content -->
+  <q-drawer show-if-above v-bind="drawerOpen" side="left" bordered>
+    <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+      <div class="absolute-bottom bg-transparent">
+        <q-avatar size="56px" class="q-mb-sm">
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+        <div class="text-weight-bold">Razvan Stoenescu</div>
+        <div>@rstoenescu</div>
+      </div>
+    </q-img>
+    <q-scroll-area q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+      <q-list padding>
+        <template v-for="(item, index) in drawerMenu" :key="index">
+          <q-item clickable :active="item.label === 'Outbox'" v-ripple>
+            <q-item-section avatar>
+              <q-icon :name="item.icon" />
+            </q-item-section>
+            <q-item-section>
+              {{ item.label }}
+            </q-item-section>
+          </q-item>
+          <q-separator :key="'sep' + index"  v-if="item.separator" />
+        </template>
+      </q-list>
+    </q-scroll-area>
   </q-drawer>
 </template>
 
@@ -11,7 +34,8 @@
 export default {
   name: "comp-side-bar",
   props: {
-    leftDrawerOpen: { type: Boolean },
+    drawerOpen: { type: Boolean },
+    drawerMenu: { type: Array },
   },
 };
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh lpR lFf">
     <comp-header :toggleLeftDrawer="toggleLeftDrawer" />
-    <comp-side-bar v-model="leftDrawerOpen" />
+    <comp-side-bar v-model="drawerOpen" :drawerMenu="drawerMenu" />
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -26,12 +26,50 @@ export default {
   },
   data() {
     return {
-      leftDrawerOpen: false,
+      drawerOpen: false,
+      drawerMenu: [
+        {
+          icon: 'inbox',
+          label: 'Inbox',
+          separator: true
+        },
+        {
+          icon: 'send',
+          label: 'Outbox',
+          separator: false
+        },
+        {
+          icon: 'delete',
+          label: 'Trash',
+          separator: false
+        },
+        {
+          icon: 'error',
+          label: 'Spam',
+          separator: true
+        },
+        {
+          icon: 'settings',
+          label: 'Settings',
+          separator: false
+        },
+        {
+          icon: 'feedback',
+          label: 'Send Feedback',
+          separator: false
+        },
+        {
+          icon: 'help',
+          iconColor: 'primary',
+          label: 'Help',
+          separator: false
+        },
+      ],
     }
   },
   methods: {
     toggleLeftDrawer() {
-      this.leftDrawerOpen = !this.leftDrawerOpen;
+      this.drawerOpen = !this.drawerOpen;
     },
   },
 };
