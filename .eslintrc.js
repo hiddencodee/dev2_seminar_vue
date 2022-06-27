208abf29b6783958219a3aa590a8013a43d38d1f
rainbow01734 <90245392+rainbow01734@users.noreply.github.com>
2022-06-27 10:40:33 +0900

    [수정] : 문서에 lint 사용 방법 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     // vue
     // https://eslint.vuejs.org/rules/html-closing-bracket-newline.html
     // 'plugin:vue/vue3-essential',           // Lv1
-    "plugin:vue/vue3-strongly-recommended",   //Lv2
+    "plugin:vue/vue3-strongly-recommended",   // Lv2
     // 'plugin:vue/vue3-recommended',         // Lv3
     // js
     // https://eslint.org/docs/rules/
