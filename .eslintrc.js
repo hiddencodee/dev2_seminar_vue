50b2cabe2e101d18c77778deccdf92ea275a06c2
JAKIM <jakim@pangpany.co.kr>
2022-07-10 00:34:44 +0900

    [추가] : eslintc.js 파일추가하여 lint적용

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    // vue
+    // https://eslint.vuejs.org/rules/html-closing-bracket-newline.html
+    // 'plugin:vue/vue3-essential',           // Lv1
+    "plugin:vue/vue3-strongly-recommended",   // Lv2
+    // 'plugin:vue/vue3-recommended',         // Lv3
+    // js
+    // https://eslint.org/docs/rules/
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "never",
+      },
+    ],
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+};
